f1ac02b128379b79b14cf2f885aeb3df371da2b8
李林城 <1019034271@qq.com>
2024-03-20 15:27:02 +0800

    fix: 解决js运算精度bug

--- preload.js
+++ preload.js
@@ -1,3 +1,4 @@
+let records = [];
 window.exports = {
     "calc": { // 注意：键对应的是 plugin.json 中的 features.code
        mode: "list",  // 列表模式
@@ -5,7 +6,16 @@ window.exports = {
           // 进入插件应用时调用（可选）
           enter: (action, callbackSetList) => {
              // 如果进入插件应用就要显示列表数据
-             callbackSetList([])
+             callbackSetList([
+              ...records.map( item => ({
+                title: item.expression,
+                description: ' = ' + item.value,
+                expression: item.expression,
+                value: item.value,
+              }))
+             ])
+             // 引入js计算类，解决精度问题
+             require('./calculator.js');
           },
           // 子输入框内容变化时被调用 可选 (未设置则无搜索)
           search: (action, searchWord, callbackSetList) => {
@@ -88,22 +98,40 @@ window.exports = {
                      case '+':
                        b = stack.shift();
                        a = stack.shift();
-                       stack.unshift( a + b );
+                      //  var b_len = (b.toString().split('.')[1] || '').length;
+                      //  var a_len = (a.toString().split('.')[1] || '').length;
+                      //  var multiple = Math.pow(10 , Math.max(b_len , a_len) );
+                      //  var b_num = b * multiple;
+                      //  var a_num = a * multiple;
+                      //  stack.unshift( ( a_num + b_num ) / multiple );
+                       stack.unshift(math.add(a , b));
                        break;
                      case '-':
                        b = stack.shift();
                        a = stack.shift();
-                       stack.unshift( a - b );
+                      //  var b_len = (b.toString().split('.')[1] || '').length;
+                      //  var a_len = (a.toString().split('.')[1] || '').length;
+                      //  var multiple = Math.pow(10 , Math.max(b_len , a_len));
+                      //  stack.unshift(( a * multiple - b * multiple ) / multiple );
+                       stack.unshift(math.sub(a , b));
                        break;
                      case '*':
                        b = stack.shift();
                        a = stack.shift();
-                       stack.unshift( a * b );
+                      //  var b_len = (b.toString().split('.')[1] || '').length;
+                      //  var a_len = (a.toString().split('.')[1] || '').length;
+                      //  var multiple = Math.pow(10 , Math.max(b_len , a_len));
+                      //  stack.unshift( (a * multiple) * (b * multiple) / ( multiple * multiple ) ) ;
+                       stack.unshift(math.mul(a , b));
                        break;
                      case '/':
                        b = stack.shift();
                        a = stack.shift();
-                       stack.unshift( a / b );
+                      //  var b_len = (b.toString().split('.')[1] || '').length;
+                      //  var a_len = (a.toString().split('.')[1] || '').length;
+                      //  var multiple = Math.pow(10 , Math.max(b_len , a_len));
+                      //  stack.unshift( ( a * multiple ) / (b * multiple) / ( multiple * multiple ) );
+                       stack.unshift(math.div(a , b));
                        break;
                      case '%':
                        b = stack.shift();
@@ -123,33 +151,48 @@ window.exports = {
                callbackSetList([
                   {
                      title: result,
-                     description: '复制计算结果',
+                     description: searchWord + ' = ' + result,
                      icon:'', // 图标
+                     key: 'result',
+                     expression: searchWord,
+                     value: result,
                   },
                   {
                     title: result,
                     description: "继续下一步计算",
                     icon:'', // 图标
-                    key: "result",
+                    key: "next",
+                    expression: searchWord,
+                    value: result,
                   },
-                  {
-                    title: searchWord + " = " + result,
-                    description: "原表达式",
-                  },
-                  {
-                    title: expression + " = " + result,
-                    description: "等价表达式",
-                  },
+                  ...records.map( item => ({
+                    title: item.expression,
+                    description: ' = ' + item.value,
+                    expression: item.expression,
+                    value: item.value,
+                  }))
                ])
              }
           },
           // 用户选择列表中某个条目时被调用
           select: (action, itemData, callbackSetList) => {
-             const { key , title } = itemData;
-             if( key === 'result' ){
+             const { key , title , expression , value} = itemData;
+             if( key === 'next' ){
                utools.setSubInputValue(title);
+               records.unshift({
+                value: title + '',
+                expression: expression,
+              })
+              records = records.slice(0, 5);
              }else{
-               utools.copyText(title+'')
+               if( key === 'result'){
+                records.unshift({
+                  value: title,
+                  expression: expression,
+                })
+                records = records.slice(0, 5);
+               }
+               utools.copyText(value+'')
                utools.hideMainWindow();
                utools.outPlugin()
              }
@@ -158,4 +201,7 @@ window.exports = {
           placeholder: "请输入正确的运算表达式"
        } 
     }
- }
+}
+
+
+ 
